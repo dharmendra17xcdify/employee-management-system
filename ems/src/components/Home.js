2a56af6763f6484589c513751a12b292d048e330
Dharmendra Yadav <dharmendra17893@gmail.com>
2018-07-22 15:28:04 +0530

    implemented list employee and map employee detail on select

--- ems/src/components/Home.js
+++ ems/src/components/Home.js
@@ -1,7 +1,72 @@
 import React, { Component } from 'react';
-
+import _ from 'lodash';
 import withAuthorization from './withAuthorization';
 import { db } from '../firebase';
+
+const employees = ['Dharmendra', 'Vaibhav', 'Yogesh', 'Srivats', 'Anurag'];
+
+const employeeData = [
+    {
+        name: 'Dharmendra',
+        fullName: 'Dharmendra Yadav',
+        imageUrl: 'images/authors/marktwain.jpg',
+        dob: '17/08/1993',
+        email: 'dharmendra17893@gmail.com',
+        mobile: 8108401991,
+        experience: '1 Year',
+        doj: '01/06/2017'
+
+    },
+    {
+        name: 'Vaibhav',
+        fullName: 'Vaibhav Agrawal',
+        imageUrl: 'images/authors/marktwain.jpg',
+        dob: '17/08/1993',
+        email: 'dharmendra17893@gmail.com',
+        mobile: 8108401991,
+        experience: '1 Year',
+        doj: '01/06/2017'
+    },
+    {
+        name: 'Yogesh',
+        fullName: 'Yogesh Dhami',
+        imageUrl: 'images/authors/marktwain.jpg',
+        dob: '17/08/1993',
+        email: 'dharmendra17893@gmail.com',
+        mobile: 8108401991,
+        experience: '1 Year',
+        doj: '01/06/2017'
+    },
+    {
+        name: 'Srivats',
+        fullName: 'Srivats Krishnan',
+        imageUrl: 'images/authors/marktwain.jpg',
+        dob: '17/08/1993',
+        email: 'dharmendra17893@gmail.com',
+        mobile: 8108401991,
+        experience: '1 Year',
+        doj: '01/06/2017'
+    },
+    {
+        name: 'Anurag',
+        fullName: 'Anurag Maratha',
+        imageUrl: 'images/authors/marktwain.jpg',
+        dob: '17 Aug 1993',
+        email: 'dharmendra17893@gmail.com',
+        mobile: 8108401991,
+        experience: '1 Year',
+        doj: '01/06/2017'
+    }
+];
+
+function Employee ({title, onClick}) {
+    return (<div className="answer">
+    <h4 value={title} data-id={title}>{title}</h4>
+    </div>
+    );
+  }
+
+  
 
 class HomePage extends Component {
   constructor(props) {
@@ -9,6 +74,17 @@ class HomePage extends Component {
 
     this.state = {
       users: null,
+      name: '',
+      employeeDetails: {
+        name: 'Dharmendra',
+        fullName: 'Dharmendra Yadav',
+        imageUrl: 'images/authors/marktwain.jpg',
+        dob: '17/08/1993',
+        email: 'dharmendra17893@gmail.com',
+        mobile: 8108401991,
+        experience: '1 Year',
+        doj: '01/06/2017'
+      }
     };
   }
 
@@ -16,6 +92,25 @@ class HomePage extends Component {
     db.onceGetUsers().then(snapshot =>
       this.setState(() => ({ users: snapshot.val() }))
     );
+  }
+
+  handleClick = (e, data) => {
+    // access to e.target here
+    console.log(e.target.innerText);
+    let emp = _.find(employeeData, {name: e.target.innerText})
+
+    this.setState({
+        employeeDetails: {
+            name: emp.name,
+            fullName: emp.fullName,
+            imageUrl: emp.imageUrl,
+            dob: emp.dob,
+            email: emp.email,
+            mobile: emp.mobile,
+            experience: emp.experience,
+            doj: emp.doj
+        }
+    });
   }
 
   render() {
@@ -36,17 +131,19 @@ class HomePage extends Component {
                             </span>
                         </div>
                         <ul className="nav nav-pills nav-stacked">
-                            <li><a href ="">Employee</a></li>
-                            <li><a href ="">Employee</a></li>
+                            <li value={employees} onClick={this.handleClick.bind(this)}>{employees.map((title) => <Employee title={title} key={title}/>)}</li>
                         </ul><br></br>
-                        
                     </div>
                     <div className="col-sm-9 jumbotron">
                         <div className="col-sm-4">
                             <div className="panel panel-primary">
                                 <div className="panel-heading">Employee Details</div>
                                 <div className="panel-body">
-                                Panel Content
+                                Name: {this.state.employeeDetails.fullName}<br/>
+                                Email: {this.state.employeeDetails.email}<br/>
+                                Mobile: {this.state.employeeDetails.mobile}<br/>
+                                DOJ: {this.state.employeeDetails.doj}<br/>
+                                DOB: {this.state.employeeDetails.dob}
                                 </div>
                             </div>
                         </div>
